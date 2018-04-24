11b4cbf807c90a165dbb0c496d4e28709ecf750e
thuong <1451010189thuong@ou.edu.vn>
2018-04-24 14:14:07 +0700

    ++update sua tin

--- app/Http/Controllers/AdminControllers/TinTucController.php
+++ app/Http/Controllers/AdminControllers/TinTucController.php
@@ -18,8 +18,19 @@ class TinTucController extends Controller
             'name' => $name
         ]);
     }
+
     function getThem() {
         return view('adminls.tintuc.them');
+    }
+
+    function getSua($id) {
+        $tintuc = TinTuc::find($id);
+        if(!$tintuc) {
+            return;
+        }
+        return view('adminls.tintuc.sua', [
+            'tintuc' => $tintuc
+        ]);
     }
     function getXoa($id) {
         $tintuc = TinTuc::find($id);
@@ -82,8 +93,59 @@ class TinTucController extends Controller
 		return redirect('admin/tintuc/them')->with('thongbao','thêm thành công');
     }
 
-    function postSua($id ,Request $request) {
+    function postSua(Request $request, $id) {
         
+    	$this->validate($request,[
+            'tieude' => 'required|min:10',
+            'mota' =>'required',
+            'noidung' => 'required',
+    	],
+    	[
+            'tieude.required' =>'bạn chưa nhập tiêu đề',
+            'tieude.min' =>'tiêu đề ít nhất 10 ký tự',
+            'mota.required' =>'bạn chưa nhập tóm tắt',
+            'noidung.required' =>'bạn chưa nhập nội dung',
+        ]);
+        $tintuc = TinTuc::find($id);
+        if(!$tintuc) {
+            return;
+        }
+        $tintuc->title = $request->tieude;
+        $tintuc->slug = str_slug($request->tieude);
+        $tintuc->des = $request->mota;
+        $tintuc->content =  $request->noidung;
+
+         //kiem tra upload anh
+         if($request->hasFile('anh')) { 
+            if ($request->file('anh')->isValid()) {                
+                $file = $request->file('anh');
+                $format =strtolower( $file->getClientOriginalExtension());
+                    if($format !='jpg' && $format !='png' && $format !='jpeg' && $format !='bmp'  ) {
+                        alert()->error('Có lỗi', 'Error');
+                        return redirect('admin/tintuc/sua/'.$id)->with('loi','file ảnh phải có đuôi jpg, png, jpeg, bmp');
+                    }             
+                    //lay url hinh
+                    $tenhinh = $file->getClientOriginalName();                    
+                    $hinh = str_random(4)."_".$tenhinh;
+                    //neu random trung thi chay lai random lai
+                    while (file_exists("img/news/".$hinh)) {
+                        $hinh = str_random(4)."_".$tenhinh;
+                    }
+                    //luu hinh vào thu muc
+                    $file->move('img/news', $hinh);
+                    unlink($tintuc->img);
+                    $tintuc->img = strtolower('img/news/'.$hinh);
+                }
+                else
+                return redirect('admin/tintuc/them')->with('loi','Lỗi, vui lòng kiểm tra lại');                
+            }        
+         else
+         {             
+             
+         }         
+		$tintuc->save();
+		//thêm thành công thì làm gì?
+		return redirect('admin/tintuc/sua/'.$id)->with('thongbao','Sửa thành công');
     }
 }
 
