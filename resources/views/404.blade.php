a37c87903187668658073f8c22485518ade9a131
thuong <1451010189thuong@ou.edu.vn>
2018-04-24 18:46:29 +0700

    ++add page 404

---
+++ resources/views/404.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js" lang="">
+<!--<![endif]-->
+	@include('templates.libary.head')
+<body class="">
+	<div id="container">    
+        @yield('main-content')        
+    </div>
+    @include('templates.libary.foot')
+</body>
+</html>
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,14 @@
+@extends('404')
+@section('main-content')
+    <div class="error-page">
+        <div class="container">
+            <h2 class="headline text-yellow text-center text-danger"> 404</h2>
+            <div class="error-content text-center">
+                <h3><i class="fa fa-warning text-yellow"></i> Oops! Trang bạn yêu cầu không khả dụng.</h3>       
+                <p>                   
+                    <a href="/">Trở về trang chủ</a> 
+                </p>                 
+            </div>
+        </div>      
+    </div>
+@stop
