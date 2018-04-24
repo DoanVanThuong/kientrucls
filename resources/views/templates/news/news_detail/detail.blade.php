8ec585e23aa949d0a5eb6b27d9f56f606d1169d6
thuong <1451010189thuong@ou.edu.vn>
2018-04-24 20:10:37 +0700

    ++add button share

--- resources/views/templates/news/news_detail/detail.blade.php
+++ resources/views/templates/news/news_detail/detail.blade.php
@@ -2,22 +2,16 @@
         <div class="container">
             <div class="row">
             <h3 class="title text-center">{{$dataNews->title}}</h3>
-                <ul class="list-inline list-social">
-                    <li>
-                        <a role="button" class="btn btn-just-icon btn-facebook" href="https://www.facebook.com/sharer/sharer.php?u={{$dataNews->slug}}" target="_blank">
-                            <i class="fa fa-facebook" arial-hidden="false"></i>
-                        </a>   
-                    </li>
-                    <li>
-                        <button class="btn btn-just-icon btn-google">
-                                <i class="fa fa-google" arial-hidden="false"></i>
-                        </button>
-                    </li>
-                   
-                </ul>
+               
                 <div class="detail">
                     {!!$dataNews->content!!}
                 </div>
             </div>
         </div>
    </section>
+
+@section('define-js')
+  <!-- Go to www.addthis.com/dashboard to customize your tools -->
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5adf2b4bd1fd9d8e"></script>
+
+@stop
