ba26397850f2718d7edbe66918eff2ee35802566
= <thuong.doan@xtravel.vn>
2018-05-24 16:52:25 +0700

    ++fix slick slider detail product

--- resources/views/templates/products/products_detail/content.blade.php
+++ resources/views/templates/products/products_detail/content.blade.php
@@ -84,6 +84,8 @@
 
  @section('define-js')
   <!-- Go to www.addthis.com/dashboard to customize your tools -->
+<script src="{{asset('js/product_detail.js')}}"></script>
+  
 <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5adf2b4bd1fd9d8e"></script>
 
 @stop
